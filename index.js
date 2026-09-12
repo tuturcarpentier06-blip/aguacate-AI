b3f544f565f348de8880d3166667de2a65a0d0c5
tuturcarpentier06-blip <tuturcarpentier06@gmail.com>
2026-09-12 11:57:03 +0200

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-// index.js - starts the express app from server.js
-const app = require('./server');
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`🥑 Aguacate AI listening on port ${PORT}`);
-});
